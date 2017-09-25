6199c4e79b364aaf8d0e054f68cba37b6da48299
Travis Petersen <travis.e.petersen@gmail.com>
2017-09-25 22:58:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function viewCart() {
   }
   else if(cart.length === 2) {
     var str = `In your cart, you have ${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]} and
-              ${Object.keys(cart[1])} at $${cart[1][Object.keys(cart[1])]}`
+              ${Object.keys(cart[1])} at $${cart[1][Object.keys(cart[1])]}.`
     console.log(str)
   }
 }
